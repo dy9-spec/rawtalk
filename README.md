01dd51776bafb1c0ba2300d832244e610e82f7cd
dy9-spec <di.yao.think@gmail.com>
2021-07-10 18:02:43 -0700

    set up AWS creds with rusoto core

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# rawtalk
+# Set up - MacOS
+
+1. Add environment variables `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY`
+   - check `~/.aws/credentials` which is the default place where aws credentials are stored on your machine
+   - if `AWS CLI` is not set up yet, please follow: https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-configure.html
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use rusoto_credential::{EnvironmentProvider, ProvideAwsCredentials};
+
+#[tokio::main]
+async fn main() {
+    // gain AWS credentials via env vars
+    let aws_creds = EnvironmentProvider::default().credentials().await.unwrap();
+    let aws_access_key = aws_creds.aws_access_key_id();
+    let aws_secret_access_key = aws_creds.aws_secret_access_key();
+
+    println!("AWS access key: {}", aws_access_key);
+    println!("AWS secret access key: {}", aws_secret_access_key);
 }
